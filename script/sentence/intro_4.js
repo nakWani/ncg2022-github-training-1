694d793d75ebcad56384120101f37792839edbb4
Taro.Nonoyama <taro.nonoyama@access-company.com>
2022-03-23 22:24:37 +0900

    Feature/add intro4 (#3)
    * add Dockerfile (#1)
    
    * reset.cssとcommon.cssを修正 (#2)
    
    * intro4の追加
    
    Co-authored-by: Ryoka.Oishi <ryoka.oishi@access-company.com>

---
+++ script/sentence/intro_4.js
@@ -0,0 +1,12 @@
+/* Input introduce yourself using Markdown.*/
+/* How to use Markdown? > https://qiita.com/tbpgr/items/989c6badefff69377da7 */
+
+/* Plz insert \n\ if you want generate new line.*/
+
+const Intro_4_Text = "\
+# YOUR NAME  \n\
+\n\
+Here is some example **formatting** in _Markdown_.  \n\
+\n\
+The following JavaScript will transform this content to clean HTML.  \n\
+"
