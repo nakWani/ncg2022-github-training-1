195d6523f9df34108f423ecaf1e32747f452989d
aDaikiWatanabe <daiki.watanabe@access-company.com>
2022-03-31 15:43:53 +0900

    [feat] impl pokeka. (#6)
    * [feat] impl pokeka.
    
    * [bug] fix typo.

--- script/sentence/intro_1.js
+++ script/sentence/intro_1.js
@@ -9,4 +9,15 @@ const Intro_1_Text = "\
 Here is some example **formatting** in _Markdown_.  \n\
 \n\
 The following JavaScript will transform this content to clean HTML.  \n\
+# あなたのポケモンカードのやりたさに関する所感 (10行) \n\
+foo1 \n\
+foo2 \n\
+foo3 \n\
+4 \n\
+5 \n\
+6 \n\
+7 \n\
+8 \n\
+99 \n\
+10 \n\
 "
